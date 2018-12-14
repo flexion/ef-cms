78f17d05bbe6dd83967d1c4e7adb1af2cb50abca
Eric Sorenson <ericsorenson@Erics-MacBook-Pro.local>
2018-12-13 19:52:42 -0600

    application context parity

--- web-client/src/applicationContexts/dev.js
+++ web-client/src/applicationContexts/dev.js
@@ -1,22 +1,22 @@
 const {
+  getDocument,
+  uploadDocument,
   uploadPdf,
   uploadPdfsForNewCase,
-  uploadDocument,
-  getDocument,
 } = require('../../../shared/src/persistence/awsS3Persistence');
 
 import { createCase } from '../../../shared/src/proxies/createCaseProxy';
+import { downloadDocumentFile } from '../../../shared/src/business/useCases/downloadDocumentFile.interactor';
+import { fileAnswer } from '../../../shared/src/business/useCases/respondent/fileAnswer.interactor';
+import { fileStipulatedDecision } from '../../../shared/src/business/useCases/respondent/fileStipulatedDecision.interactor';
 import { getCase } from '../../../shared/src/proxies/getCaseProxy';
 import { getCasesByStatus } from '../../../shared/src/proxies/getCasesByStatusProxy';
 import { getCasesByUser } from '../../../shared/src/proxies/getCasesByUserProxy';
+import { getCasesForRespondent } from '../../../shared/src/proxies/respondent/getCasesForRespondentProxy';
 import { getUser } from '../../../shared/src/business/useCases/getUser.interactor';
 import { sendPetitionToIRS } from '../../../shared/src/proxies/sendPetitionToIRSProxy';
 import { updateCase } from '../../../shared/src/proxies/updateCaseProxy';
 import { uploadCasePdfs } from '../../../shared/src/business/useCases/uploadCasePdfs.interactor';
-import { downloadDocumentFile } from '../../../shared/src/business/useCases/downloadDocumentFile.interactor';
-import { fileAnswer } from '../../../shared/src/business/useCases/respondent/fileAnswer.interactor';
-import { getCasesForRespondent } from '../../../shared/src/proxies/respondent/getCasesForRespondentProxy';
-import { fileStipulatedDecision } from '../../../shared/src/business/useCases/respondent/fileStipulatedDecision.interactor';
 
 import Case from '../../../shared/src/business/entities/Case';
 
@@ -29,26 +29,26 @@ const applicationContext = {
   },
   getPersistenceGateway: () => {
     return {
-      uploadPdf,
-      uploadPdfsForNewCase,
-      uploadDocument,
       getDocument,
       saveCase: updateCase,
+      uploadDocument,
+      uploadPdf,
+      uploadPdfsForNewCase,
     };
   },
   getUseCases: () => {
     return {
       createCase,
+      downloadDocumentFile,
+      fileAnswer,
       getCase,
       getCasesByStatus,
       getCasesByUser,
+      getCasesForRespondent,
       getUser,
       sendPetitionToIRS,
       updateCase,
       uploadCasePdfs,
-      fileAnswer,
-      getCasesForRespondent,
-      downloadDocumentFile,
     };
   },
   getUseCaseForDocumentUpload: (documentType, role) => {
--- web-client/src/applicationContexts/prod.js
+++ web-client/src/applicationContexts/prod.js
@@ -1,22 +1,22 @@
 const {
+  getDocument,
+  uploadDocument,
   uploadPdf,
   uploadPdfsForNewCase,
-  uploadDocument,
-  getDocument,
 } = require('../../../shared/src/persistence/awsS3Persistence');
 
 import { createCase } from '../../../shared/src/proxies/createCaseProxy';
+import { downloadDocumentFile } from '../../../shared/src/business/useCases/downloadDocumentFile.interactor';
+import { fileAnswer } from '../../../shared/src/business/useCases/respondent/fileAnswer.interactor';
+import { fileStipulatedDecision } from '../../../shared/src/business/useCases/respondent/fileStipulatedDecision.interactor';
 import { getCase } from '../../../shared/src/proxies/getCaseProxy';
 import { getCasesByStatus } from '../../../shared/src/proxies/getCasesByStatusProxy';
 import { getCasesByUser } from '../../../shared/src/proxies/getCasesByUserProxy';
+import { getCasesForRespondent } from '../../../shared/src/proxies/respondent/getCasesForRespondentProxy';
 import { getUser } from '../../../shared/src/business/useCases/getUser.interactor';
 import { sendPetitionToIRS } from '../../../shared/src/proxies/sendPetitionToIRSProxy';
 import { updateCase } from '../../../shared/src/proxies/updateCaseProxy';
 import { uploadCasePdfs } from '../../../shared/src/business/useCases/uploadCasePdfs.interactor';
-import { fileAnswer } from '../../../shared/src/business/useCases/respondent/fileAnswer.interactor';
-import { getCasesForRespondent } from '../../../shared/src/proxies/respondent/getCasesForRespondentProxy';
-import { downloadDocumentFile } from '../../../shared/src/business/useCases/downloadDocumentFile.interactor';
-import { fileStipulatedDecision } from '../../../shared/src/business/useCases/respondent/fileStipulatedDecision.interactor';
 
 import Case from '../../../shared/src/business/entities/Case';
 
@@ -29,28 +29,30 @@ const applicationContext = {
   },
   getPersistenceGateway: () => {
     return {
-      uploadPdf,
       getDocument,
-      uploadPdfsForNewCase,
+      saveCase: updateCase,
       uploadDocument,
+      uploadPdf,
+      uploadPdfsForNewCase,
     };
   },
   getUseCases: () => {
     return {
       createCase,
+      downloadDocumentFile,
+      fileAnswer,
       getCase,
       getCasesByStatus,
       getCasesByUser,
+      getCasesForRespondent,
       getUser,
       sendPetitionToIRS,
       updateCase,
       uploadCasePdfs,
-      fileAnswer,
-      getCasesForRespondent,
-      downloadDocumentFile,
     };
   },
   getUseCaseForDocumentUpload: (documentType, role) => {
+    // TODO put user in so we can remove role
     if (role === 'respondent') {
       switch (documentType) {
         case Case.documentTypes.answer:
