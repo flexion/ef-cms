c33a5b672c83bc55ba6f2ceb389c8a76fb84fadf
Eric Sorenson <eric@sorenson.software>
2019-06-18 15:42:59 -0500

    remove deprecated @babel/polyfill

--- web-client/src/index.dev.js
+++ web-client/src/index.dev.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import Devtools from 'cerebral/devtools';
 
 import { Case } from '../../shared/src/business/entities/Case';
