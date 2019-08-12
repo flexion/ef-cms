7656e746894df58259950ba8840d1908de1aceb9
Kris Koskelin <kkoskelin@flexion.us>
2019-08-12 13:25:26 -0500

    2565: trying to fix merge conflicts

--- web-client/src/views/TrialSessionWorkingCopy/TrialSessionWorkingCopy.jsx
+++ web-client/src/views/TrialSessionWorkingCopy/TrialSessionWorkingCopy.jsx
@@ -1,19 +1,24 @@
+import { BigHeader } from '../BigHeader';
 import { ErrorNotification } from '../ErrorNotification';
 import { SuccessNotification } from '../SuccessNotification';
 import { TrialSessionDetailHeader } from '../TrialSessionDetail/TrialSessionDetailHeader';
-import { connect } from '@cerebral/react';
+import { WorkingCopySessionList } from './WorkingCopySessionList';
+import { connect, state } from '@cerebral/react';
 import React from 'react';
 
 export const TrialSessionWorkingCopy = connect(
-  {},
-  () => {
+  {
+    title: state.trialSessionWorkingCopyHelper.title,
+  },
+  ({ title }) => {
     return (
       <>
         <TrialSessionDetailHeader />
+        <BigHeader text={title} />
         <section className="usa-section grid-container">
           <SuccessNotification />
           <ErrorNotification />
-          <p>Trial Session Working Copy</p>
+          <WorkingCopySessionList />
         </section>
       </>
     );
