795e0d29a72f06834c426df8b99f322d4c9a1bd0
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2019-08-23 08:04:59 -0500

    439: close header menus when clicking outside

--- web-client/src/views/ReportsMenu.jsx
+++ web-client/src/views/ReportsMenu.jsx
@@ -1,16 +1,43 @@
 import { connect } from '@cerebral/react';
 import { sequences, state } from 'cerebral';
+import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 
-export const ReportsMenu = connect(
-  {
-    pageIsReports: state.headerHelper.pageIsReports,
-    toggleReportsMenuSequence: sequences.toggleReportsMenuSequence,
-  },
-  ({ isExpanded, pageIsReports, toggleReportsMenuSequence }) => {
+export class ReportsMenuComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.resetHeaderAccordionsSequence = props.resetHeaderAccordionsSequence;
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentWillMount() {
+    document.addEventListener('mousedown', this.handleClick, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClick, false);
+  }
+
+  handleClick(e) {
+    if (this.node.contains(e.target)) {
+      return;
+    }
+    this.handleClickOutside();
+  }
+
+  handleClickOutside() {
+    this.resetHeaderAccordionsSequence();
+  }
+
+  render() {
+    const { isExpanded, pageIsReports, toggleReportsMenuSequence } = this.props;
+
     return (
-      <>
+      <div ref={node => (this.node = node)}>
         <button
           aria-expanded={isExpanded}
           className={classNames(
@@ -30,7 +57,23 @@ export const ReportsMenu = connect(
             </li>
           </ul>
         )}
-      </>
+      </div>
     );
+  }
+}
+
+ReportsMenuComponent.propTypes = {
+  isExpanded: PropTypes.bool,
+  pageIsReports: PropTypes.bool,
+  resetHeaderAccordionsSequence: PropTypes.func,
+  toggleReportsMenuSequence: PropTypes.func,
+};
+
+export const ReportsMenu = connect(
+  {
+    pageIsReports: state.headerHelper.pageIsReports,
+    resetHeaderAccordionsSequence: sequences.resetHeaderAccordionsSequence,
+    toggleReportsMenuSequence: sequences.toggleReportsMenuSequence,
   },
+  ReportsMenuComponent,
 );
