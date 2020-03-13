42a34f0c1db79ce755bd43369cf5752d83794080
Cody Seibert <cseibert113@gmail.com>
2020-03-13 09:57:15 -0400

    fixing judge test

--- web-client/integration-tests/journey/petitionerAddNewCaseToTestObj.js
+++ web-client/integration-tests/journey/petitionerAddNewCaseToTestObj.js
@@ -3,6 +3,9 @@ export default (test, createdCases) => {
     await test.runSequence('gotoDashboardSequence');
     const petitionerNewCase = test.getState('cases.0');
     expect(petitionerNewCase).toBeDefined();
+    expect(petitionerNewCase.documents).toBeDefined();
+    expect(expect(petitionerNewCase.documents[0])).toBeDefined();
+    expect(expect(petitionerNewCase.documents[0].workItems)).toBeDefined();
     expect(petitionerNewCase).toEqual('a');
     expect(petitionerNewCase.documents[0].workItems[0]).toBeDefined();
     createdCases.push(petitionerNewCase);
