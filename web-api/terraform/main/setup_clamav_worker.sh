4ab2260f5f88abcd7be4bc7132cafe227b3920a0
Rachel Schneiderman <rschneiderman@flexion.us>
2020-08-20 12:21:41 -0700

    6053 [skip ci] Updating setup_clamav_worker, rearranging how clamav is started and definitions are updated

--- web-api/terraform/main/setup_clamav_worker.sh
+++ web-api/terraform/main/setup_clamav_worker.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 quarantine_bucket=${quarantine_bucket}
 clean_documents_bucket=${documents_bucket_name}
-sqs_queue=${sqs_queue}
 sqs_queue_url=${sqs_queue_url}
 environment=${environment}
 monitor_script_s3_path=${monitor_script_s3_path}
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update
-sudo apt install -y awscli nodejs clamav-base clamav clamav-daemon clamav-freshclam
+sudo apt install -y awscli nodejs clamav clamav-daemon
+
+sudo mkdir /var/run/clamav/
 
 # TODO: config
-# TODO: double check clamav daemon autostarts
+sudo mkdir /usr/local/etc/clamav
+sudo touch /usr/local/etc/clamav/freshclam.conf 
+sudo cat <<< '
+LocalSocket /tmp/clamd.socket
+LocalSocketMode 660
+' > /usr/local/etc/clamav/freshclam.conf
+
+sudo chmod 775 /usr/local/etc/clamav
+
+sudo chown -R clamav:clamav /var/run/clamav/
 
 sudo freshclam # update definitions
-sudo clamd # start daemon
+
+sudo systemctl start clamav-daemon.service
 
 sudo aws s3 cp "${monitor_script_s3_path}" monitor.js
 
 npm i -g pm2
 
 # start monitoring
-sudo ENV=${environment} SQS_QUEUE=${sqs_queue} QUARANTINE_BUCKET=${quarantine_bucket} pm2 monitor.js
+sudo ENV=${environment} SQS_QUEUE_URL=${sqs_queue_url} QUARANTINE_BUCKET=${quarantine_bucket} pm2 monitor.js
