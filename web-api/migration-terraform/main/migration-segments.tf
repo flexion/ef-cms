58fcf9786d82cdb396165b7beeac7138bf675ca6
Rachael Parris <43251054+rachaelparris@users.noreply.github.com>
2020-11-19 08:39:28 -0800

    Increase timeout for migration queue and lambda

--- web-api/migration-terraform/main/migration-segments.tf
+++ web-api/migration-terraform/main/migration-segments.tf
@@ -13,8 +13,8 @@ resource "aws_lambda_function" "migration_segments_lambda" {
   source_code_hash = data.archive_file.migration_segments_zip.output_base64sha256
 
   runtime     = "nodejs12.x"
-  timeout     = "60"
-  memory_size = "768"
+  timeout     = "900"
+  memory_size = "3008"
 
   environment {
     variables = {
