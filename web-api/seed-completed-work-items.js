2a22f3522dec6c0c6a63ad4fdbd90f67c16f2b25
Kris Koskelin <kkoskelin@flexion.us>
2021-02-16 14:26:43 -0600

    7730: create faker data for testing [skip ci]

--- web-api/seed-completed-work-items.js
+++ web-api/seed-completed-work-items.js
@@ -17,79 +17,86 @@ console.log('tablename:', tableName);
 
 const dynamo = new AWS.DynamoDB({
   credentials: {
-    accessKeyId: 'noop',
-    secretAccessKey: 'noop',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
-  endpoint: 'http://localhost:8000',
+  endpoint: 'dynamodb.us-east-1.amazonaws.com',
   region: 'us-east-1',
 });
 
 console.log('started dynamo instance');
 
 const documentClient = new AWS.DynamoDB.DocumentClient({
-  endpoint: 'http://localhost:8000',
+  endpoint: 'dynamodb.us-east-1.amazonaws.com',
   region: 'us-east-1',
   service: dynamo,
 });
 
 console.log('created document client');
 
-const workItems = times(25, () => ({
-  assigneeId: '1805d1ab-18d0-43ec-bafb-654e83405416',
-  assigneeName: 'Test Docketclerk',
-  caseIsInProgress: true,
-  caseStatus: 'New',
-  completedBy: 'Test Docketclerk',
-  completedByUserId: '1805d1ab-18d0-43ec-bafb-654e83405416',
-  createdAt: `${faker.date.between('2021-02-16', '2021-02-09')}`,
-  docketNumber: '106-19',
-  docketNumberSuffix: null,
-  document: {
-    category: 'Answer (filed by respondent only)',
-    createdAt: '2019-07-12T17:11:26.955Z',
-    docketNumber: '106-19',
-    documentId: `${faker.random.uuid()}`,
-    documentTitle: 'Answer',
-    documentType: 'Answer',
-    eventCode: 'A',
-    filedBy: 'Petr. Denise Gould',
-    filingDate: '1990-10-10',
-    isPaper: true,
-    lodged: false,
-    numberOfPages: 1,
-    partyPrimary: true,
-    practitioner: [],
-    processingStatus: 'pending',
-    receivedAt: '1990-10-10',
-    relationship: 'primaryDocument',
-    scenario: 'Standard',
-    serviceDate: null,
-    userId: `${faker.random.uuid()}`,
-  },
-  gsi1pk: `work-item|${faker.random.uuid()}`,
-  pk: 'section-outbox|docket',
-  section: 'docket',
-  sentBy: 'Test Docketclerk',
-  sentBySection: 'docket',
-  sentByUserId: '1805d1ab-18d0-43ec-bafb-654e83405416',
-  sk: `${faker.date.between('2021-02-16', '2021-02-09')}`,
-  updatedAt: '2019-07-12T17:11:27.244Z',
-  workItemId: faker.random.uuid(),
-}));
-
-console.log('created workitems', workItems.length);
-
 (async function () {
-  console.log('about to batch write');
-  await documentClient
-    .batchWrite({
-      RequestItems: {
-        [tableName]: workItems.map(item => ({
-          PutRequest: {
-            Item: { ...item, indexedTimestamp: Date.now() },
-          },
-        })),
+  for (let i = 0; i < 110; i++) {
+    console.log('Creating ', 25 * (i + 1));
+    const workItems = times(25, () => ({
+      ...{
+        docketNumber: '106-19',
+        gsi1pk: `work-item|${faker.random.uuid()}`,
+        pk: 'section-outbox|docket',
+        sk: `${faker.date.between('2021-02-16', '2021-02-09').toISOString()}`,
       },
-    })
-    .promise();
+      assigneeId: '1805d1ab-18d0-43ec-bafb-654e83405416',
+      assigneeName: 'Test Docketclerk',
+      caseIsInProgress: true,
+      caseStatus: 'New',
+      caseTitle: `FAKER ${faker.company.catchPhrase()}`,
+      completedAt: `${faker.date
+        .between('2021-02-16', '2021-02-09')
+        .toISOString()}`,
+      completedBy: 'Test Docketclerk',
+      completedByUserId: '1805d1ab-18d0-43ec-bafb-654e83405416',
+      createdAt: `${faker.date
+        .between('2021-02-16', '2021-02-09')
+        .toISOString()}`,
+      docketEntry: {
+        category: 'Answer (filed by respondent only)',
+        createdAt: '2019-07-12T17:11:26.955Z',
+        docketNumber: '106-19',
+        documentId: `${faker.random.uuid()}`,
+        documentTitle: `FAKER ${faker.company.catchPhrase()}`,
+        documentType: 'Answer',
+        eventCode: 'A',
+        filedBy: 'Petr. Denise Gould',
+        filingDate: '1990-10-10',
+        isPaper: true,
+        lodged: false,
+        numberOfPages: 1,
+        partyPrimary: true,
+        practitioner: [],
+        processingStatus: 'pending',
+        receivedAt: '1990-10-10',
+        relationship: 'primaryDocument',
+        scenario: 'Standard',
+        serviceDate: null,
+        userId: `${faker.random.uuid()}`,
+      },
+      docketNumberSuffix: null,
+      section: 'docket',
+      sentBy: 'Test Docketclerk',
+      sentBySection: 'docket',
+      sentByUserId: '1805d1ab-18d0-43ec-bafb-654e83405416',
+      updatedAt: '2019-07-12T17:11:27.244Z',
+      workItemId: faker.random.uuid(),
+    }));
+    await documentClient
+      .batchWrite({
+        RequestItems: {
+          [tableName]: workItems.map(item => ({
+            PutRequest: {
+              Item: { ...item, indexedTimestamp: Date.now() },
+            },
+          })),
+        },
+      })
+      .promise();
+  }
 })();
