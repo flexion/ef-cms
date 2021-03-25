b125ddab8cf34ee91b5f65ac5764f15927241e13
Kris Koskelin <kkoskelin@flexion.us>
2021-03-25 15:53:14 -0500

    8051: enforce strict ISO formatting on receivedAt
    always store calendar day as start of day EST.

--- shared/src/business/entities/DocketEntry.js
+++ shared/src/business/entities/DocketEntry.js
@@ -10,13 +10,16 @@ const {
   TRACKED_DOCUMENT_TYPES_EVENT_CODES,
 } = require('./EntityConstants');
 const {
+  createISODateAtStartOfDayEST,
+  createISODateString,
+} = require('../utilities/DateHandler');
+const {
   DOCKET_ENTRY_VALIDATION_RULES,
 } = require('./EntityValidationConstants');
 const {
   joiValidationDecorator,
   validEntityDecorator,
 } = require('../../utilities/JoiValidationDecorator');
-const { createISODateString } = require('../utilities/DateHandler');
 const { User } = require('./User');
 const { WorkItem } = require('./WorkItem');
 
@@ -124,7 +127,7 @@ DocketEntry.prototype.init = function init(
   this.partyPrimary = rawDocketEntry.partyPrimary;
   this.partySecondary = rawDocketEntry.partySecondary;
   this.processingStatus = rawDocketEntry.processingStatus || 'pending';
-  this.receivedAt = createISODateString(rawDocketEntry.receivedAt);
+  this.receivedAt = createISODateAtStartOfDayEST(rawDocketEntry.receivedAt);
   this.relationship = rawDocketEntry.relationship;
   this.scenario = rawDocketEntry.scenario;
   if (rawDocketEntry.scenario === 'Nonstandard H') {
