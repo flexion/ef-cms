1d6eaf99fa5f5c4473d7fccb13eb053076b6252b
Joseph Sutton <jsutton@flexion.us>
2021-06-08 09:59:34 -0500

    8326: added streams

--- web-api/scripts/flatCaseDocketEntryIndex.js
+++ web-api/scripts/flatCaseDocketEntryIndex.js
@@ -1,4 +1,4 @@
-const { chunk } = require('lodash');
+const ElasticsearchScrollStream = require('elasticsearch-scroll-stream');
 const { getClient } = require('../elasticsearch/client');
 
 const environmentName = process.argv[2] || 'exp1';
@@ -8,65 +8,59 @@ const findDocketEntries = async () => {
   const esClient = await getClient({ environmentName, version });
 
   let allDocketEntries = [];
-  const responseQueue = [];
 
-  // start things off by searching, setting a scroll timeout, and pushing
-  // our first response into the queue to be processed
-  const response = await esClient.search({
-    body: {
-      _source: ['*', 'parent.*', 'case-mappings.*'],
-      query: {
-        bool: {
-          filter: [{ term: { 'entityName.S': 'DocketEntry' } }],
-          must: [
-            {
-              has_parent: {
-                inner_hits: {
-                  _source: ['*'],
-                  name: 'case-mappings',
+  const esStream = new ElasticsearchScrollStream(
+    esClient,
+    {
+      body: {
+        _source: ['*', 'parent.*', 'case-mappings.*'],
+        query: {
+          bool: {
+            filter: [{ term: { 'entityName.S': 'DocketEntry' } }],
+            must: [
+              {
+                has_parent: {
+                  inner_hits: {
+                    _source: ['*'],
+                    name: 'case-mappings',
+                  },
+                  parent_type: 'case',
+                  query: { match_all: {} },
+                  score: true,
                 },
-                parent_type: 'case',
-                query: { match_all: {} },
-                score: true,
               },
-            },
-          ],
+            ],
+          },
         },
       },
+      index: 'efcms-docket-entry',
+      scroll: '30s',
+      size: 500,
     },
-    index: 'efcms-docket-entry',
-    scroll: '30s',
-    size: 4000,
-  });
+    ['inner_hits'],
+  );
 
-  console.log('Got a response.');
+  console.log('Stream created');
 
-  responseQueue.push(response);
+  esStream.on('data', async data => {
+    const docketEntryDocument = JSON.parse(data.toString());
+    const caseMapping =
+      docketEntryDocument['inner_hits']['case-mappings'].hits.hits[0][
+        '_source'
+      ];
 
-  while (responseQueue.length) {
-    const body = responseQueue.shift();
-
-    // collect the titles from this response
-    body.hits.hits.forEach(function (hit) {
-      allDocketEntries.push({
-        ...hit['_source'],
-        ...hit['inner_hits']['case-mappings'].hits.hits[0]['_source'],
-        entityName: { S: 'DocketEntry' },
-        isCaseSealed: {
-          BOOL: !!hit['inner_hits']['case-mappings'].hits.hits[0]['_source']
-            .isSealed?.BOOL,
-        },
-        sk: { S: `docket-entry|${hit['_source'].docketEntryId.S}` },
-      });
+    allDocketEntries.push({
+      ...docketEntryDocument,
+      ...caseMapping,
+      entityName: { S: 'DocketEntry' },
+      isCaseSealed: {
+        BOOL: !!caseMapping.isSealed?.BOOL,
+      },
+      sk: { S: `docket-entry|${docketEntryDocument.docketEntryId.S}` },
     });
 
-    const failedRecords = [];
-
-    const chunks = chunk(allDocketEntries, 1000);
-    let done = 0;
-    let promises = [];
-    for (let chunkOfRecords of chunks) {
-      const indexBody = chunkOfRecords
+    if (allDocketEntries.length === 500) {
+      const body = allDocketEntries
         .flatMap(doc => {
           const index = 'efcms-docket-entry-no-parent';
           let id = `${doc.pk.S}_${doc.sk.S}`;
@@ -83,42 +77,20 @@ const findDocketEntries = async () => {
         })
         .filter(item => item);
 
-      if (indexBody.length) {
-        promises.push(
-          esClient.bulk({
-            body: indexBody,
-            refresh: false,
-          }),
-        );
+      esClient.bulk({
+        body,
+        refresh: false,
+      });
 
-        console.log('Sent a bulk: ', ++done, chunks.length);
+      console.log('Sent bulk of 500');
 
-        allDocketEntries = [];
-
-        // if (resp.errors) {
-        //   resp.items.forEach((action, i) => {
-        //     const operation = Object.keys(action)[0];
-        //     if (action[operation].error) {
-        //       let record = body[i * 2 + 1];
-        //       failedRecords.push(record);
-        //     }
-        //   });
-        // }
-      }
+      allDocketEntries = [];
     }
+  });
 
-    await Promise.all(promises);
-
-    console.log('failedRecords', failedRecords);
-
-    // get the next response if there are more quotes to fetch
-    responseQueue.push(
-      await esClient.scroll({
-        scroll: '30s',
-        scrollId: body['_scroll_id'],
-      }),
-    );
-  }
+  esStream.on('end', () => {
+    console.log('done');
+  });
 };
 (async () => {
   await findDocketEntries();
