35d88785ab6908cf294e7b77b7adcf9b8d4d271d
rosie highsmith <ahighsmith@flexion.us>
2021-06-15 11:54:11 -0700

    devex-925: cleanup test, accessing iframe body now, still not getting address-label inside it to scroll and grab screenshot.

--- cypress/support/pages/case-detail.js
+++ cypress/support/pages/case-detail.js
@@ -77,22 +77,21 @@ exports.getPrintPaperServiceConfirmationButton = () => {
   return cy.get('button#confirm');
 };
 
-exports.getPdfPreviewUrl = () => {
-  // return cy
-  //   .get('#pdf-preview-iframe')
-  //   .invoke('attr', 'src')
-  //   .then($style1 => {
-  //     const pdfUrl = $style1;
-  //     console.log(pdfUrl);
-  //   });
-
-  return cy.get('#pdf-preview-iframe').then($iframe => {
-    const iframe = $iframe.contents();
-    console.log(iframe);
-    const myInput = iframe.find('.address-label');
-    return cy.get(myInput).matchImageSnapshot(myInput);
-
-    //you don't need to trigger events like keyup or change
-  });
-  // return cy.get('#pdf-preview-iframe').attr('src');
+const getIframeDocument = () => {
+  return cy.get('#pdf-preview-iframe').its('0.contentDocument').should('exist');
 };
+
+exports.getIframeBody = () => {
+  const woo = getIframeDocument()
+    .its('body')
+    .should('not.be.undefined')
+    .then(cy.wrap);
+
+  cy.get('#pdf-preview-iframe')
+    .its('0')
+    .then(x => console.log('woo', x));
+
+  return woo;
+};
+
+// cy.get('.address-label').scrollIntoView().should('be.visible')
