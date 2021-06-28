c904a4112279ba6ef14ebb2e578d1800281e06b6
rosie highsmith <ahighsmith@flexion.us>
2021-06-28 09:48:14 -0700

    8345: experimenting with ckeditor

--- web-client/src/views/CreateOrder/TextEditor.jsx
+++ web-client/src/views/CreateOrder/TextEditor.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/prop-types */
 import 'react-quill/dist/quill.snow.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
 import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
+import { editCorrespondenceDocumentSequence } from '../../presenter/sequences/editCorrespondenceDocumentSequence';
+import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 import React, { Suspense, useEffect, useRef } from 'react';
 import reactQuill from 'react-quill';
 
@@ -49,8 +52,62 @@ export const TextEditor = ({
 
   return (
     <>
+      {/* <script src="https://cdn.ckeditor.com/ckeditor5/28.0.0/decoupled-document/ckeditor.js"></script> */}
+      <CKEditor
+        data={editorDelta || defaultValue}
+        editor={DecoupledEditor}
+        onChange={(event, editor) => console.log({ editor, event })}
+        // onChange={(content, delta, source, editor) => {
+        //   console.log();
+        //   const fullDelta = editor.getContents();
+        //   const documentContents = editor.getText();
+        //   const converter = new QuillDeltaToHtmlConverter(fullDelta.ops, {
+        //     inlineStyles: {
+        //       size: inlineStylesFontSizes,
+        //     },
+        //   });
+        //   const html = converter.convert();
+        //   updateFormValueSequence({
+        //     key: 'richText',
+        //     value: html,
+        //   });
+        //   updateFormValueSequence({
+        //     key: 'editorDelta',
+        //     value: fullDelta,
+        //   });
+        //   updateFormValueSequence({
+        //     key: 'documentContents',
+        //     value: documentContents,
+        //   });
+        //   updateScreenMetadataSequence({
+        //     key: 'pristine',
+        //     value: false,
+        //   });
+        // }}
+        onError={({ willEditorRestart }) => {
+          // If the editor is restarted, the toolbar element will be created once again.
+          // The `onReady` callback will be called again and the new toolbar will be added.
+          // This is why you need to remove the older toolbar.
+          if (willEditorRestart) {
+            this.editor.ui.view.toolbar.element.remove();
+          }
+        }}
+        onReady={editor => {
+          console.log('Editor is ready to use!', editor);
+
+          // Insert the toolbar before the editable area.
+          editor.ui
+            .getEditableElement()
+            .parentElement.insertBefore(
+              editor.ui.view.toolbar.element,
+              editor.ui.getEditableElement(),
+            );
+
+          this.editor = editor;
+        }}
+      />
       <Suspense fallback={<div>Loading...</div>}>
-        <ReactQuill
+        {/* <ReactQuill
           defaultValue={editorDelta || defaultValue}
           formats={[
             'size',
@@ -104,7 +161,7 @@ export const TextEditor = ({
               value: false,
             });
           }}
-        />
+        /> */}
         <button
           aria-hidden
           className="usa-sr-only"
