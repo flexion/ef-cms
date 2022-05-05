cc9a617197498de961043a90c8577308dac25695
Cody Seibert <cseibert113@gmail.com>
2022-05-05 14:53:36 -0400

    9483 - refactoring some approaches to how we fetch judges

--- shared/src/business/useCases/users/getJudgeForUserChambersInteractor.js
+++ shared/src/business/useCases/users/getJudgeForUserChambersInteractor.js
@@ -5,37 +5,24 @@ const { ROLES } = require('../../entities/EntityConstants');
  *
  * @param {object} applicationContext the application context
  * @param {object} providers the providers object
- * @param {string} providers.user the chambers user
  * @returns {User} the judge user for the given chambers user
  */
 
-exports.getJudgeForUserChambersInteractor = async (
-  applicationContext,
-  { user },
-) => {
-  let judgeUser;
-  let chambersSection;
+exports.getJudgeForUserChambersInteractor = async applicationContext => {
+  const chamberUser = await applicationContext
+    .getUseCases()
+    .getUserInteractor(applicationContext);
+  const chambersSection = chamberUser.section;
 
-  if (user.role === ROLES.chambers || user.role === ROLES.judge) {
-    if (user.section) {
-      chambersSection = user.section;
-    } else {
-      const chamberUser = await applicationContext
-        .getUseCases()
-        .getUserInteractor(applicationContext);
-      chambersSection = chamberUser.section;
-    }
+  const sectionUsers = await applicationContext
+    .getUseCases()
+    .getUsersInSectionInteractor(applicationContext, {
+      section: chambersSection,
+    });
 
-    const sectionUsers = await applicationContext
-      .getUseCases()
-      .getUsersInSectionInteractor(applicationContext, {
-        section: chambersSection,
-      });
-
-    judgeUser = sectionUsers.find(
-      sectionUser => sectionUser.role === ROLES.judge,
-    );
-  }
+  const judgeUser = sectionUsers.find(
+    sectionUser => sectionUser.role === ROLES.judge,
+  );
 
   return judgeUser;
 };
