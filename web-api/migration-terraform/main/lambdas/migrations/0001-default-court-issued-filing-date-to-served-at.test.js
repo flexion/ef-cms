806270215a5a2d4d55dcb6761b5dc07c9726e1d4
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-12-07 09:03:53 -0800

    9745: Add test to verify filedBy is not overwritten when creating new DocketEntry without specifiying petitioners [skip ci]

--- web-api/migration-terraform/main/lambdas/migrations/0001-default-court-issued-filing-date-to-served-at.test.js
+++ web-api/migration-terraform/main/lambdas/migrations/0001-default-court-issued-filing-date-to-served-at.test.js
@@ -30,4 +30,39 @@ describe('migrateItems', () => {
 
     expect(results[0].servedAt).toEqual(results[0].filingDate);
   });
+
+  it('should NOT modify any other properties on a served, court-issued docket entry other than filingDate', async () => {
+    const mockServedAt = '2020-07-17T19:28:29.675Z';
+    const mockDocketEntry = {
+      ...MOCK_DOCUMENTS[0],
+      eventCode: 'O',
+      filedBy: 'Bernard Lowe, Petitioner',
+      filingDate: undefined,
+      servedAt: mockServedAt,
+      servedParties: [
+        {
+          name: 'Bernard Lowe',
+        },
+        {
+          name: 'IRS',
+          role: 'irsSuperuser',
+        },
+      ],
+    };
+
+    const items = [
+      {
+        pk: `case|${MOCK_CASE.docketNumber}`,
+        sk: 'docket-entry|83b77e98-4cf6-4fb4-b8c0-f5f90fd68f3c',
+        ...mockDocketEntry,
+      },
+    ];
+
+    const results = await migrateItems(items, documentClient);
+
+    expect(results[0]).toEqual({
+      ...mockDocketEntry,
+      filingDate: mockServedAt,
+    });
+  });
 });
