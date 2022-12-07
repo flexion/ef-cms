c238606434c131604485f3a2f94d07e34eb35a46
Jason Fu <jfu@flexion.us>
2022-12-07 08:39:41 -0800

    9745 - add preliminary stuff; update migrationsToRun

--- web-api/migration-terraform/main/lambdas/migrations/0001-default-court-issued-filing-date-to-served-at.js
+++ web-api/migration-terraform/main/lambdas/migrations/0001-default-court-issued-filing-date-to-served-at.js
@@ -1,16 +1,22 @@
-// const isCaseRecord = item => {
-//   return item.pk.startsWith('case|') && item.sk.startsWith('case|');
-// };
+// const DocketEntry = require('../../../../../shared/src/business/entities/DocketEntry');
+// const {
+//   COURT_ISSUED_EVENT_CODES,
+// } = require('../../../../../shared/src/business/entities/EntityConstants');
+// const applicationContext = require('../../../../src/applicationContext');
+
+const isDocketEntryItem = item => {
+  return item.pk.startsWith('case|') && item.sk.startsWith('docket-entry|');
+};
 
 const migrateItems = items => {
-  // const itemsAfter = [];
-  // for (const item of items) {
-  //   if (isCaseRecord(item)) {
-  //     delete item.hasSealedDocuments;
-  //   }
-  //   itemsAfter.push(item);
-  // }
-  // return itemsAfter;
+  const itemsAfter = [];
+  for (const item of items) {
+    if (isDocketEntryItem(item)) {
+      // const docketEntry = new DocketEntry({ item, applicationContext });
+    }
+    itemsAfter.push(item);
+  }
+  return itemsAfter;
 };
 
 exports.migrateItems = migrateItems;
--- web-api/migration-terraform/main/lambdas/migrationsToRun.js
+++ web-api/migration-terraform/main/lambdas/migrationsToRun.js
@@ -1,14 +1,13 @@
 const {
   migrateItems: migration0001,
-} = require('./migrations/0001-remove-has-sealed-documents');
-const {
-  migrateItems: migration0002,
-} = require('./migrations/0002-default-sealed-to');
+} = require('./migrations/0001-default-court-issued-filing-date-to-served-at');
 
 // MODIFY THIS ARRAY TO ADD NEW MIGRATIONS OR REMOVE OLD ONES
 const migrationsToRun = [
-  { key: '0001-remove-has-sealed-documents.js', script: migration0001 },
-  { key: '0002-default-sealed-to.js', script: migration0002 },
+  {
+    key: '0001-default-court-issued-filing-date-to-served-at.js',
+    script: migration0001,
+  },
 ];
 
 exports.migrationsToRun = migrationsToRun;
