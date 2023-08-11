2824d357d23b3f1619e7d0ff19e3625b11db071f
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2023-08-11 15:30:01 -0500

    10116: reduce name of health check

--- web-api/terraform/api/failover.tf
+++ web-api/terraform/api/failover.tf
@@ -2,7 +2,7 @@ resource "aws_route53_health_check" "failover_health_check" {
   // fqdn must be a fully qualified domain name, and the invoke_url is not a domain but a url.
   // Therefore, we are addding the stage ("/exp1") to the resource path, and omitting ("https://") from the fqdn
   // e.g: https://6oz2qiqb7h.execute-api.us-east-1.amazonaws.com/exp1 --> 6oz2qiqb7h.execute-api.us-east-1.amazonaws.com
-  reference_name     = "${var.environment} ${var.current_color} Failover Health Check"
+  reference_name     = "${var.environment} ${var.current_color} Failover"
   fqdn               = element(split("/", aws_api_gateway_stage.api_public_stage.invoke_url), 2)
   port               = 443
   type               = "HTTPS_STR_MATCH"
