17ad4b92bcb65ba642d31dba21711b585b030c76
Absolutestunna <joshujah007@gmail.com>
2024-03-26 10:26:10 -0400

    seperating the creation of cases with downloading of cases

--- cypress/cypress-integration/integration/batch-download-documents.cy.ts
+++ cypress/cypress-integration/integration/batch-download-documents.cy.ts
@@ -5,6 +5,7 @@ import {
   loginAsDocketClerk,
   loginAsPetitioner,
 } from '../../helpers/auth/login-as-helpers';
+import { navigateToCase } from '../support/pages/petition-qc';
 import { petitionerCreatesElectronicCase } from '../../helpers/petitioner-creates-electronic-case';
 import { petitionsClerkQcsAndServesElectronicCase } from '../../helpers/petitions-clerk-qcs-and-serves-electronic-case';
 import { retry } from '../../helpers/retry';
@@ -74,208 +75,213 @@ describe('Batch Download Documents', () => {
   //   cy.task('verifyExistenceOfFolder', downloadPath);
   // });
 
-  // afterEach(() => {
-  //   const downloadPath = Cypress.config('downloadsFolder');
-  //   cy.task('deleteAllFilesInFolder', downloadPath);
-  // });
+  afterEach(() => {
+    const downloadPath = Cypress.config('downloadsFolder');
+    cy.task('deleteAllFilesInFolder', downloadPath);
+  });
+
+  let zipName: string;
+  let documentsCreatedWithCase;
+  let caseNumberCreated: string;
+
+  it('should create case for validating downloaded documents', () => {
+    createAndServePaperPetition().then(
+      ({ docketNumber, documentsCreated, name }) => {
+        documentsCreatedWithCase = documentsCreated;
+        zipName = `${docketNumber}, ${name}.zip`;
+        caseNumberCreated = docketNumber;
+      },
+    );
+  });
 
   it('should download all eligible documents that are not stricken with the printable docket record, with all files in a flat directory', () => {
-    createAndServePaperPetition().then(
-      ({ docketNumber, documentsCreated, name }) => {
-        const downloadPath = Cypress.config('downloadsFolder');
+    const downloadPath = Cypress.config('downloadsFolder');
+    let expectedFileCount: number = documentsCreatedWithCase.length;
+    const expectedSealedCount = 0;
+    const expectedStrickenCount = 0;
 
-        const zipName = `${docketNumber}, ${name}.zip`;
-        let expectedFileCount = documentsCreated.length;
-        const expectedSealedCount = 0;
-        const expectedStrickenCount = 0;
-        searchByDocketNumberInHeader(docketNumber);
-        confirmCountOfDocumentsToDownload(documentsCreated.length);
-        includePrintableDocketRecord();
-        expectedFileCount++;
-        cy.get('[data-testid="modal-button-confirm"]').click();
+    navigateToCase('petitionsclerk', caseNumberCreated);
+    searchByDocketNumberInHeader(caseNumberCreated);
+    confirmCountOfDocumentsToDownload(documentsCreatedWithCase.length);
+    includePrintableDocketRecord();
+    expectedFileCount++;
+    cy.get('[data-testid="modal-button-confirm"]').click();
 
-        retry(() => verifyDownloadedFiles({ downloadPath, zipName }));
+    retry(() => verifyDownloadedFiles({ downloadPath, zipName }));
 
-        cy.unzipFile({
-          destinationPath: downloadPath,
-          filePath: `${downloadPath}/${zipName}`,
-        }).then(files => {
-          checkFileCounts({
-            expectedFileCount,
-            expectedSealedCount,
-            expectedStrickenCount,
-            files,
-          });
-        });
-      },
-    );
-  });
-
-  it('should download all eligible documents with some stricken and some sealed, labeling files and adding sealed docs to sealed directory', () => {
-    createAndServePaperPetition().then(
-      ({ docketNumber, documentsCreated, name }) => {
-        const downloadPath = Cypress.config('downloadsFolder');
-
-        const zipName = `${docketNumber}, ${name}.zip`;
-        const expectedFileCount = documentsCreated.length;
-        let expectedSealedCount = 0;
-        let expectedStrickenCount = 0;
-        loginAsDocketClerk();
-        searchByDocketNumberInHeader(docketNumber);
-
-        // strike documents
-        cy.get('[data-testid="edit-ATP"]').click();
-        cy.get('[data-testid="tab-action"]').click();
-        cy.get('[data-testid="strike-entry"]').click();
-        cy.get('[data-testid="modal-button-confirm"]').click();
-        expectedStrickenCount++;
-
-        cy.get('[data-testid="edit-APW"]').click();
-        cy.get('[data-testid="tab-action"]').click();
-        cy.get('[data-testid="strike-entry"]').click();
-        cy.get('[data-testid="modal-button-confirm"]').click();
-        expectedStrickenCount++;
-
-        // seal documents
-        cy.get('[data-testid="seal-docket-entry-button-3"]').click();
-        cy.get('[data-testid="modal-button-confirm"]').click();
-        expectedSealedCount++;
-
-        cy.get('[data-testid="seal-docket-entry-button-5"]').click();
-        cy.get('[data-testid="modal-button-confirm"]').click();
-        expectedSealedCount++;
-
-        confirmCountOfDocumentsToDownload(documentsCreated.length);
-        cy.get('[data-testid="modal-button-confirm"]').click();
-
-        retry(() => verifyDownloadedFiles({ downloadPath, zipName }));
-
-        cy.unzipFile({
-          destinationPath: downloadPath,
-          filePath: `${downloadPath}/${zipName}`,
-        }).then(files => {
-          checkFileCounts({
-            expectedFileCount,
-            expectedSealedCount,
-            expectedStrickenCount,
-            files,
-          });
-        });
-      },
-    );
-  });
-
-  it('should download selected documents, labeling stricken documents and putting everything except the printable docket record into the sealed directory when then the case is sealed', () => {
-    createAndServePaperPetition().then(
-      ({ docketNumber, documentsCreated, name }) => {
-        const downloadPath = Cypress.config('downloadsFolder');
-
-        const zipName = `${docketNumber}, ${name}.zip`;
-        let expectedFileCount = documentsCreated.length;
-        let expectedSealedCount = 0;
-        let expectedStrickenCount = 0;
-        loginAsDocketClerk();
-        searchByDocketNumberInHeader(docketNumber);
-
-        // strike documents
-        cy.get('[data-testid="edit-ATP"]').click();
-        cy.get('[data-testid="tab-action"]').click();
-        cy.get('[data-testid="strike-entry"]').click();
-        cy.get('[data-testid="modal-button-confirm"]').click();
-        expectedStrickenCount++;
-
-        cy.get('[data-testid="edit-APW"]').click();
-        cy.get('[data-testid="tab-action"]').click();
-        cy.get('[data-testid="strike-entry"]').click();
-        cy.get('[data-testid="modal-button-confirm"]').click();
-        expectedStrickenCount++;
-
-        // seal entire case
-        cy.get('[data-testid="tab-case-information"]').click();
-        cy.get('[data-testid="seal-case-button"]').click();
-        cy.get('[data-testid="modal-button-confirm"]').click();
-        cy.get('[data-testid="tab-docket-record"] > .button-text').click();
-        expectedSealedCount = documentsCreated.length;
-
-        confirmCountOfDocumentsToDownload(documentsCreated.length);
-        includePrintableDocketRecord();
-        expectedFileCount++;
-        cy.get('[data-testid="modal-button-confirm"]').click();
-
-        retry(() => verifyDownloadedFiles({ downloadPath, zipName }));
-
-        cy.unzipFile({
-          destinationPath: downloadPath,
-          filePath: `${downloadPath}/${zipName}`,
-        }).then(files => {
-          checkFileCounts({
-            expectedFileCount,
-            expectedSealedCount,
-            expectedStrickenCount,
-            files,
-          });
-        });
-      },
-    );
-  });
-
-  it('should download selected documents filtered by document type', () => {
-    createAndServePaperPetition().then(({ docketNumber, name }) => {
-      const downloadPath = Cypress.config('downloadsFolder');
-
-      const zipName = `${docketNumber}, ${name}.zip`;
-      const documentsFilteredByDocumentType = [{ eventCode: 'O' }];
-      let expectedFileCount = 1;
-      let expectedSealedCount = 0;
-      let expectedStrickenCount = 0;
-      const today = createISODateString();
-      loginAsDocketClerk();
-      searchByDocketNumberInHeader(docketNumber);
-      cy.get('[data-testid="tab-drafts"] > .button-text').click();
-      cy.get('[data-testid="docket-entry-description-1"]').click();
-      cy.get('#apply-signature').click();
-      cy.get('[data-testid="sign-pdf-canvas"]').click();
-      cy.get('[data-testid="save-signature-button"]').click();
-      cy.get('[data-testid="add-court-issued-docket-entry-button"]').click();
-      cy.get('.usa-date-picker__wrapper > [data-testid="date-picker"]').type(
-        today,
-      );
-      cy.get('[data-testid="serve-to-parties-btn"]').click();
-      cy.get('[data-testid="modal-button-confirm"]').click();
-      cy.get('[data-testid="print-paper-service-done-button"]').click();
-      cy.get('#document-filter-by').select('Orders');
-      confirmCountOfDocumentsToDownload(documentsFilteredByDocumentType.length);
-
-      cy.get('[data-testid="modal-button-confirm"]').click();
-
-      retry(() => verifyDownloadedFiles({ downloadPath, zipName }));
-
-      cy.unzipFile({
-        destinationPath: downloadPath,
-        filePath: `${downloadPath}/${zipName}`,
-      }).then(files => {
-        checkFileCounts({
-          expectedFileCount,
-          expectedSealedCount,
-          expectedStrickenCount,
-          files,
-        });
+    cy.unzipFile({
+      destinationPath: downloadPath,
+      filePath: `${downloadPath}/${zipName}`,
+    }).then(files => {
+      checkFileCounts({
+        expectedFileCount,
+        expectedSealedCount,
+        expectedStrickenCount,
+        files,
       });
     });
   });
 
-  it('should not show download controls for external users', () => {
-    loginAsPetitioner();
-    petitionerCreatesElectronicCase().then(docketNumber => {
-      petitionsClerkQcsAndServesElectronicCase(docketNumber);
-      loginAsPetitioner();
-      externalUserSearchesDocketNumber(docketNumber);
-      cy.get('[data-testid="download-docket-records-button"]').should(
-        'not.exist',
-      );
-      cy.get('[data-testid="all-selectable-docket-entries-checkbox"]').should(
-        'not.exist',
-      );
+  it('should download all eligible documents with some stricken and some sealed, labeling files and adding sealed docs to sealed directory', () => {
+    const downloadPath = Cypress.config('downloadsFolder');
+    const expectedFileCount: number = documentsCreatedWithCase.length;
+    let expectedSealedCount = 0;
+    let expectedStrickenCount = 0;
+
+    loginAsDocketClerk();
+    searchByDocketNumberInHeader(caseNumberCreated);
+
+    // strike documents
+    cy.get('[data-testid="edit-ATP"]').click();
+    cy.get('[data-testid="tab-action"]').click();
+    cy.get('[data-testid="strike-entry"]').click();
+    cy.get('[data-testid="modal-button-confirm"]').click();
+    expectedStrickenCount++;
+
+    cy.get('[data-testid="edit-APW"]').click();
+    cy.get('[data-testid="tab-action"]').click();
+    cy.get('[data-testid="strike-entry"]').click();
+    cy.get('[data-testid="modal-button-confirm"]').click();
+    expectedStrickenCount++;
+
+    // seal documents
+    cy.get('[data-testid="seal-docket-entry-button-3"]').click();
+    cy.get('[data-testid="modal-button-confirm"]').click();
+    expectedSealedCount++;
+
+    cy.get('[data-testid="seal-docket-entry-button-5"]').click();
+    cy.get('[data-testid="modal-button-confirm"]').click();
+    expectedSealedCount++;
+
+    confirmCountOfDocumentsToDownload(documentsCreatedWithCase.length);
+    cy.get('[data-testid="modal-button-confirm"]').click();
+
+    retry(() => verifyDownloadedFiles({ downloadPath, zipName }));
+
+    cy.unzipFile({
+      destinationPath: downloadPath,
+      filePath: `${downloadPath}/${zipName}`,
+    }).then(files => {
+      checkFileCounts({
+        expectedFileCount,
+        expectedSealedCount,
+        expectedStrickenCount,
+        files,
+      });
     });
   });
+
+  // it('should download selected documents, labeling stricken documents and putting everything except the printable docket record into the sealed directory when then the case is sealed', () => {
+  //   createAndServePaperPetition().then(
+  //     ({ docketNumber, documentsCreated, name }) => {
+  //       const downloadPath = Cypress.config('downloadsFolder');
+
+  //       const zipName = `${docketNumber}, ${name}.zip`;
+  //       let expectedFileCount = documentsCreated.length;
+  //       let expectedSealedCount = 0;
+  //       let expectedStrickenCount = 0;
+  //       loginAsDocketClerk();
+  //       searchByDocketNumberInHeader(docketNumber);
+
+  //       // strike documents
+  //       cy.get('[data-testid="edit-ATP"]').click();
+  //       cy.get('[data-testid="tab-action"]').click();
+  //       cy.get('[data-testid="strike-entry"]').click();
+  //       cy.get('[data-testid="modal-button-confirm"]').click();
+  //       expectedStrickenCount++;
+
+  //       cy.get('[data-testid="edit-APW"]').click();
+  //       cy.get('[data-testid="tab-action"]').click();
+  //       cy.get('[data-testid="strike-entry"]').click();
+  //       cy.get('[data-testid="modal-button-confirm"]').click();
+  //       expectedStrickenCount++;
+
+  //       // seal entire case
+  //       cy.get('[data-testid="tab-case-information"]').click();
+  //       cy.get('[data-testid="seal-case-button"]').click();
+  //       cy.get('[data-testid="modal-button-confirm"]').click();
+  //       cy.get('[data-testid="tab-docket-record"] > .button-text').click();
+  //       expectedSealedCount = documentsCreated.length;
+
+  //       confirmCountOfDocumentsToDownload(documentsCreated.length);
+  //       includePrintableDocketRecord();
+  //       expectedFileCount++;
+  //       cy.get('[data-testid="modal-button-confirm"]').click();
+
+  //       retry(() => verifyDownloadedFiles({ downloadPath, zipName }));
+
+  //       cy.unzipFile({
+  //         destinationPath: downloadPath,
+  //         filePath: `${downloadPath}/${zipName}`,
+  //       }).then(files => {
+  //         checkFileCounts({
+  //           expectedFileCount,
+  //           expectedSealedCount,
+  //           expectedStrickenCount,
+  //           files,
+  //         });
+  //       });
+  //     },
+  //   );
+  // });
+
+  // it('should download selected documents filtered by document type', () => {
+  //   createAndServePaperPetition().then(({ docketNumber, name }) => {
+  //     const downloadPath = Cypress.config('downloadsFolder');
+
+  //     const zipName = `${docketNumber}, ${name}.zip`;
+  //     const documentsFilteredByDocumentType = [{ eventCode: 'O' }];
+  //     let expectedFileCount = 1;
+  //     let expectedSealedCount = 0;
+  //     let expectedStrickenCount = 0;
+  //     const today = createISODateString();
+  //     loginAsDocketClerk();
+  //     searchByDocketNumberInHeader(docketNumber);
+  //     cy.get('[data-testid="tab-drafts"] > .button-text').click();
+  //     cy.get('[data-testid="docket-entry-description-1"]').click();
+  //     cy.get('#apply-signature').click();
+  //     cy.get('[data-testid="sign-pdf-canvas"]').click();
+  //     cy.get('[data-testid="save-signature-button"]').click();
+  //     cy.get('[data-testid="add-court-issued-docket-entry-button"]').click();
+  //     cy.get('.usa-date-picker__wrapper > [data-testid="date-picker"]').type(
+  //       today,
+  //     );
+  //     cy.get('[data-testid="serve-to-parties-btn"]').click();
+  //     cy.get('[data-testid="modal-button-confirm"]').click();
+  //     cy.get('[data-testid="print-paper-service-done-button"]').click();
+  //     cy.get('#document-filter-by').select('Orders');
+  //     confirmCountOfDocumentsToDownload(documentsFilteredByDocumentType.length);
+
+  //     cy.get('[data-testid="modal-button-confirm"]').click();
+
+  //     retry(() => verifyDownloadedFiles({ downloadPath, zipName }));
+
+  //     cy.unzipFile({
+  //       destinationPath: downloadPath,
+  //       filePath: `${downloadPath}/${zipName}`,
+  //     }).then(files => {
+  //       checkFileCounts({
+  //         expectedFileCount,
+  //         expectedSealedCount,
+  //         expectedStrickenCount,
+  //         files,
+  //       });
+  //     });
+  //   });
+  // });
+
+  // it('should not show download controls for external users', () => {
+  //   loginAsPetitioner();
+  //   petitionerCreatesElectronicCase().then(docketNumber => {
+  //     petitionsClerkQcsAndServesElectronicCase(docketNumber);
+  //     loginAsPetitioner();
+  //     externalUserSearchesDocketNumber(docketNumber);
+  //     cy.get('[data-testid="download-docket-records-button"]').should(
+  //       'not.exist',
+  //     );
+  //     cy.get('[data-testid="all-selectable-docket-entries-checkbox"]').should(
+  //       'not.exist',
+  //     );
+  //   });
+  // });
 });
