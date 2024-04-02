826554b2cca0efa52eefac235b8325b8b225eb8d
Kaitlyn Swann <kswann@flexion.us>
2024-04-02 12:04:08 -0600

    10208: add user if they dont exist in the new user pool

--- web-api/terraform/template/lambdas/cognito-triggers.ts
+++ web-api/terraform/template/lambdas/cognito-triggers.ts
@@ -1,56 +1,84 @@
+import {
+  AdminCreateUserCommandInput,
+  AttributeType,
+  AuthFlowType,
+} from '@aws-sdk/client-cognito-identity-provider';
+import {
+  ServerApplicationContext,
+  createApplicationContext,
+} from '@web-api/applicationContext';
+
 export const handler = async event => {
-  // const AWS = require('aws-sdk');
-  // const stsclient = new AWS.STS();
-  // const createNewUser = require('./createNewUser');
-
-  // const sourceAccountRoleARN = 'ROLEARN';
-  // const sourceAccountRegion = 'REGION';
-  // const sourceAccountUserPoolId = 'USERPOOLID';
-  // const sourceAccountClientId = 'APPCLIENTID';
-  // let cognitoIdpClient;
-
-  // let user;
-
-  // const paramsAssumeRole = {
-  //   RoleArn: sourceAccountRoleARN,
-  //   RoleSessionName: 'CrossAccountCognitoMigration',
-  // };
-  // const { Credentials } = await stsclient
-  //   .assumeRole(paramsAssumeRole)
-  //   .promise();
-
-  // const tempCredentialsObj = {
-  //   accessKeyId: Credentials.AccessKeyId,
-  //   secretAccessKey: Credentials.SecretAccessKey,
-  //   sessionToken: Credentials.SessionToken,
-  // };
-
-  // AWS.config.update({ credentials: tempCredentialsObj });
-  // cognitoIdpClient = new AWS.CognitoIdentityServiceProvider({
-  //   region: sourceAccountRegion,
-  // });
+  const applicationContext: ServerApplicationContext = createApplicationContext(
+    {},
+  );
 
   if (event.triggerSource == 'UserMigration_Authentication') {
-    // authenticate the user with existing user pool
-    // user = await authenticateUser(event.userName, event.request.password);
+    // authenticate the user with old user pool
+    const result = await applicationContext.getCognito().adminInitiateAuth({
+      AuthFlow: 'ADMIN_USER_PASSWORD_AUTH',
+      AuthParameters: {
+        PASSWORD: event.request.password,
+        USERNAME: event.userName,
+      },
+      ClientId: '2em8dvalgn7eednvasqa6reok3', // OLD user pool
+      UserPoolId: 'us-east-1_jDerGoxYK', // OLD user pool
+    });
 
-    // let userAttributes = {};
+    let user;
+    if (result.hasOwnProperty('AuthenticationResult')) {
+      user = await applicationContext.getCognito().adminGetUser({
+        UserPoolId: 'us-east-1_jDerGoxYK', // OLD user pool
+        Username: event.userName,
+      });
+    }
 
-    // if (user.Username) {
-    //   user.UserAttributes.forEach(attribute => {
-    //     if (attribute.Name == 'sub') {
-    //       return;
-    //     }
-    //     userAttributes[attribute.Name] = attribute.Value;
-    //   });
+    let userAttributes: AttributeType[] = [];
+    let sub,
+      customUserId = '';
 
-    //   await createNewUser(event.userName, event.request.password);
+    if (user.Username) {
+      user.UserAttributes.forEach(attribute => {
+        if (attribute.Name == 'sub') {
+          sub = attribute.Value;
+          return;
+        }
+        if (attribute.Name === 'custom:userId') {
+          customUserId = attribute.Value;
+          return;
+        }
+        userAttributes[attribute.Name] = attribute.Value;
+      });
 
-    //   event.response.userAttributes = userAttributes;
+      if (customUserId === '') {
+        customUserId = sub;
+      }
 
-    //   event.response.finalUserStatus = 'CONFIRMED';
-    //   event.response.messageAction = 'SUPPRESS';
-    //   context.succeed(event);
+      // Create user in new user pool
+      await applicationContext.getCognito().adminCreateUser({
+        DesiredDeliveryMediums: ['EMAIL'],
+        TemporaryPassword: event.request.password,
+        UserAttributes: [
+          ...userAttributes,
+          {
+            Name: 'custom:userId',
+            Value: customUserId,
+          },
+          // {
+          //   Name: 'email_verified',
+          //   Value: 'True',
+          // },
+        ],
+        UserPoolId: process.env.USER_POOL_ID!,
+        Username: user.pendingEmail!,
+      });
+
+      // Allow user to login
+      //   event.response.userAttributes = userAttributes;
+      //   event.response.finalUserStatus = 'CONFIRMED';
+      //   event.response.messageAction = 'SUPPRESS';
+      //   context.succeed(event);
+    }
     console.log('Login - UserMigration_Authentication', event);
   } else if (event.triggerSource == 'UserMigration_ForgotPassword') {
     console.log('Forgot Password - UserMigration_ForgotPassword', event);
@@ -59,35 +87,3 @@ export const handler = async event => {
     console.log('Bad triggerSource');
   }
 };
-
-// async function getUserPoolUser(username) {
-//   let res = '';
-//   const paramGetuser = {
-//     UserPoolId: sourceAccountUserPoolId,
-//     Username: username,
-//   };
-
-//   res = await cognitoIdpClient.adminGetUser(paramGetuser).promise();
-//   return res;
-// }
-
-// async function authenticateUser(username, password) {
-//   let res = '';
-//   const paramInitiateAuth = {
-//     AuthFlow: 'ADMIN_USER_PASSWORD_AUTH',
-//     AuthParameters: {
-//       PASSWORD: password,
-//       USERNAME: username,
-//     },
-//     ClientId: sourceAccountClientId,
-//     UserPoolId: sourceAccountUserPoolId,
-//   };
-
-//   const authres = await cognitoIdpClient
-//     .adminInitiateAuth(paramInitiateAuth)
-//     .promise();
-//   if (authres.hasOwnProperty('AuthenticationResult')) {
-//     res = getUserPoolUser(username);
-//   }
-//   return res;
-// }
