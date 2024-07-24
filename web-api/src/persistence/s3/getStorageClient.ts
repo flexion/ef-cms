de50829c1cb7011b66a21112ab0a179507ca399c
Zachary Rogers <zrogers>
2024-07-24 15:08:07 -0700

    10336: WIP create many s3 clients and run them in parallel

--- web-api/src/persistence/s3/getStorageClient.ts
+++ web-api/src/persistence/s3/getStorageClient.ts
@@ -4,23 +4,22 @@ import { S3 } from '@aws-sdk/client-s3';
 import { environment } from '@web-api/environment';
 import AWSXRay from 'aws-xray-sdk';
 
-let s3Cache: S3;
-
 export function getStorageClient(): S3 {
-  if (!s3Cache) {
-    s3Cache = AWSXRay.captureAWSv3Client(
-      new S3({
-        endpoint: environment.s3Endpoint,
-        forcePathStyle: true,
-        maxAttempts: 3,
-        region: 'us-east-1',
-        requestHandler: new NodeHttpHandler({
-          connectionTimeout: 3000,
-          httpsAgent: new Agent({ keepAlive: true, maxSockets: 75 }),
-          requestTimeout: 30000,
-        }),
+  // if (!s3Cache) {
+  //   s3Cache =
+  return AWSXRay.captureAWSv3Client(
+    new S3({
+      endpoint: environment.s3Endpoint,
+      forcePathStyle: true,
+      maxAttempts: 3,
+      region: 'us-east-1',
+      requestHandler: new NodeHttpHandler({
+        connectionTimeout: 3000,
+        httpsAgent: new Agent({ keepAlive: true, maxSockets: 75 }),
+        requestTimeout: 30000,
       }),
-    );
-  }
-  return s3Cache;
+    }),
+  );
+  // }
+  // return s3Cache;
 }
--- web-api/src/persistence/s3/zipDocuments.ts
+++ web-api/src/persistence/s3/zipDocuments.ts
@@ -2,6 +2,9 @@ import { AsyncZipDeflate, Zip } from 'fflate';
 import { PassThrough, Writable } from 'stream';
 import { ServerApplicationContext } from '@web-api/applicationContext';
 import { Upload } from '@aws-sdk/lib-storage';
+import { chunk } from 'lodash';
+import { cpus } from 'os';
+import { netstat } from 'node-os-utils';
 
 /**
  *
@@ -16,10 +19,33 @@ Parallelized:
 
 76769 ms
 2173 MB
+
+Many S3 Clients. No parallelization:
+Duration: 106897.34 ms	Billed Duration: 106898 ms	Memory Size: 7000 MB	Max Memory Used: 969 MB
+Duration: 112162.03 ms	Billed Duration: 112163 ms	Memory Size: 7000 MB	Max Memory Used: 1072 MB
+Duration: 110400.68 ms	Billed Duration: 110401 ms	Memory Size: 7000 MB	Max Memory Used: 1072 MB
+Duration: 101778.89 ms	Billed Duration: 101779 ms	Memory Size: 7000 MB	Max Memory Used: 1126 MB
+Duration: 90429.72 ms	  Billed Duration: 90430 ms	  Memory Size: 7000 MB	Max Memory Used: 1459 MB
+
+Many S3 Clients. parallelization w/ 4 processes:
+Duration: 73162.39 ms	Billed Duration: 73163 ms	Memory Size: 7000 MB	Max Memory Used: 2530 MB
+Duration: 73040.62 ms	Billed Duration: 73041 ms	Memory Size: 7000 MB	Max Memory Used: 2530 MB
+Duration: 72233.40 ms	Billed Duration: 72234 ms	Memory Size: 7000 MB	Max Memory Used: 2755 MB
+Duration: 74471.14 ms	Billed Duration: 74472 ms	Memory Size: 7000 MB	Max Memory Used: 2874 MB
+Duration: 74391.22 ms	Billed Duration: 74392 ms	Memory Size: 7000 MB	Max Memory Used: 2927 MB
+
+Many S3 Clients. parallelization w/ 8 processes:
+Duration: 84925.62 ms	Billed Duration: 84926 ms	Memory Size: 7000 MB	Max Memory Used: 2554 MB
+Duration: 80978.39 ms	Billed Duration: 80979 ms	Memory Size: 7000 MB	Max Memory Used: 2823 MB
+Duration: 78094.34 ms	Billed Duration: 78095 ms	Memory Size: 7000 MB	Max Memory Used: 2902 MB
+Duration: 70474.41 ms	Billed Duration: 70475 ms	Memory Size: 7000 MB	Max Memory Used: 3025 MB
+Duration: 72388.79 ms	Billed Duration: 72389 ms	Memory Size: 7000 MB	Max Memory Used: 3143 MB
+
+Duration: 167998.84 ms	Billed Duration: 167999 ms	Memory Size: 7000 MB	Max Memory Used: 2689 MB	Init Duration: 2050.39 ms
 */
 
 /**
- * WE LEFT OFF: Brice asked the question: "Do we know we have seperate connections for upload/download?". We should explore if using multiple S3 clients for reading documents or at least upload zip/download documents improves things.
+ * WE LEFT OFF: We can measure upload speed by bypassing all of the zipping/downloading and just uploading a large file from a lambda directly.
  */
 
 export type ProgressData = {
@@ -43,7 +69,10 @@ export async function zipDocuments(
     outputZipName: string;
   },
 ): Promise<void> {
+  console.log('cpu: ', cpus());
   const passThrough = new PassThrough({ highWaterMark: 1024 * 1024 * 100 });
+  const info = await netstat.inOut();
+  console.log('Network Info', info);
 
   const upload = new Upload({
     client: applicationContext.getStorageClient(),
@@ -55,7 +84,7 @@ export async function zipDocuments(
   });
 
   const writable = new Writable({
-    write(chunk, encoding, callback) {
+    write(_chunk, encoding, callback) {
       callback();
     },
   });
@@ -78,53 +107,57 @@ export async function zipDocuments(
     }
   });
 
-  for (let index = 0; index < documents.length; index++) {
-    const document = documents[index];
+  const documentChunks = chunk(documents, 4);
+  for (let index = 0; index < documentChunks.length; index++) {
+    const documentChunk = documentChunks[index];
+    await Promise.all(
+      documentChunk.map(async document => {
+        const response = await applicationContext.getStorageClient().getObject({
+          Bucket: document.useTempBucket
+            ? applicationContext.environment.tempDocumentsBucketName
+            : applicationContext.environment.documentsBucketName,
+          Key: document.key,
+        });
+        if (!response.Body) {
+          throw new Error(
+            `Unable to get document (${document.key}) from persistence.`,
+          );
+        }
 
-    const response = await applicationContext.getStorageClient().getObject({
-      Bucket: document.useTempBucket
-        ? applicationContext.environment.tempDocumentsBucketName
-        : applicationContext.environment.documentsBucketName,
-      Key: document.key,
-    });
-    if (!response.Body) {
-      throw new Error(
-        `Unable to get document (${document.key}) from persistence.`,
-      );
-    }
+        // Transform s3 getobject into a stream of data that can be piped into the zip processor
+        const bodyStream: ReadableStream<Uint8Array> =
+          response.Body.transformToWebStream();
+        const reader = bodyStream.getReader();
+        const compressedPdfStream = new AsyncZipDeflate(document.filePathInZip);
+        zip.add(compressedPdfStream);
 
-    // Transform s3 getobject into a stream of data that can be piped into the zip processor
-    const bodyStream: ReadableStream<Uint8Array> =
-      response.Body.transformToWebStream();
-    const reader = bodyStream.getReader();
-    const compressedPdfStream = new AsyncZipDeflate(document.filePathInZip);
-    zip.add(compressedPdfStream);
+        let continueReading = true;
+        const start = Date.now();
+        while (continueReading) {
+          const unzippedChunk = await reader.read();
+          const nextChunk = unzippedChunk.value || new Uint8Array();
+          continueReading = !unzippedChunk.done;
+          compressedPdfStream.push(nextChunk, unzippedChunk.done);
+          while (passThrough.readableLength > 1024 * 1024 * 10) {
+            // Wait for the buffer to be drained, before downloading more files
+            await new Promise(resolve => setTimeout(resolve, 10));
+          }
+        }
+        const end = Date.now();
+        console.log(
+          `Done reading document ${document.key}, time elapsed: `,
+          start - end,
+        );
+        reader.releaseLock();
 
-    let continueReading = true;
-    const start = Date.now();
-    while (continueReading) {
-      const unzippedChunk = await reader.read();
-      const nextChunk = unzippedChunk.value || new Uint8Array();
-      continueReading = !unzippedChunk.done;
-      compressedPdfStream.push(nextChunk, unzippedChunk.done);
-      while (passThrough.readableLength > 1024 * 1024 * 10) {
-        // Wait for the buffer to be drained, before downloading more files
-        await new Promise(resolve => setTimeout(resolve, 10));
-      }
-    }
-    const end = Date.now();
-    console.log(
-      `Done reading document ${document.key}, time elapsed: `,
-      start - end,
+        if (onProgress) {
+          await onProgress({
+            filesCompleted: index + 1,
+            totalFiles: documents.length,
+          });
+        }
+      }),
     );
-    reader.releaseLock();
-
-    if (onProgress) {
-      await onProgress({
-        filesCompleted: index + 1,
-        totalFiles: documents.length,
-      });
-    }
   }
 
   zip.end();
