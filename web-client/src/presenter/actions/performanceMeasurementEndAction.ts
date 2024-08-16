4f4ffdfe7dc6827576f2897cd79e78db6e1cab51
John Cruz <jcruz@flexion.us>
2024-08-16 08:19:06 -0600

    10432: In the client side only send performance data once logged in;

--- web-client/src/presenter/actions/performanceMeasurementEndAction.ts
+++ web-client/src/presenter/actions/performanceMeasurementEndAction.ts
@@ -1,17 +1,25 @@
 import { DateTime } from 'luxon';
+import { state } from '@web-client/presenter/app.cerebral';
 
 export const performanceMeasurementEndAction = ({
   applicationContext,
+  get,
   props,
 }: ActionProps<{
   sequenceName?: string;
   performanceMeasurementStart?: number;
   actionPerformanceArray?: { actionName: string; duration: number }[];
 }>) => {
+  const { email } = get(state.user) || {};
   const { actionPerformanceArray, performanceMeasurementStart, sequenceName } =
     props;
 
-  if (!sequenceName || !performanceMeasurementStart || !actionPerformanceArray)
+  if (
+    !sequenceName ||
+    !performanceMeasurementStart ||
+    !actionPerformanceArray ||
+    !email
+  )
     return;
 
   const performanceMeasurementEnd = DateTime.now().toMillis();
