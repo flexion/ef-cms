8e707abf19a9c0eef7678b3c6ac9c6c656e8cd9c
John Cruz <jcruz@flexion.us>
2024-08-19 08:43:48 -0600

    10432: WIP post report data to the webhook;

--- aws/lambdas/SequencePerformanceReport/index.js
+++ aws/lambdas/SequencePerformanceReport/index.js
@@ -1,14 +1,34 @@
 const AWS = require('aws-sdk');
+const axios = require('axios');
 const secretsManager = new AWS.SecretsManager();
 
-exports.handler = async () => await SequencePerformanceReport(secretsManager);
+exports.handler = async () =>
+  await SequencePerformanceReport({
+    httpClient: axios,
+    secretsManagerClient: secretsManager,
+  });
 
-async function SequencePerformanceReport(secretsManagerClient) {
+async function getReportData() {
+  return await new Promise(resolve => resolve('John is TESTING'));
+}
+
+async function SequencePerformanceReport({ httpClient, secretsManagerClient }) {
   const secretName = 'SLACK_WEBHOOK_URL';
   const SLACK_WEBHOOK_URL = await secretsManagerClient
     .getSecretValue({ SecretId: secretName })
     .promise()
     .catch(() => null);
 
-  console.log('SLACK_WEBHOOK_URL', SLACK_WEBHOOK_URL);
+  if (!SLACK_WEBHOOK_URL) return;
+
+  const REPORT_DATA = await getReportData();
+  const BODY = { text: REPORT_DATA };
+  const OPTIONS = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  console.log('GOING TO POST REPORT TO SLACK');
+  await httpClient.post(SLACK_WEBHOOK_URL, BODY, OPTIONS).catch(() => null);
 }
