83dbb679bb6d56ca8862b82e4d630569c065c3e9
John Cruz <jcruz@flexion.us>
2024-08-19 12:05:08 -0600

    10432: Attach report lambda to cron job to run every Monday 9am MST time;

--- web-api/terraform/modules/sequence-performance-report-cron/sequence-performance-report-cron.tf
+++ web-api/terraform/modules/sequence-performance-report-cron/sequence-performance-report-cron.tf
@@ -7,7 +7,7 @@ module "sequence_performance_report_lambda_role" {
 
 resource "terraform_data" "locals" {
   input = {
-    STAGE                              = var.environment
+    STAGE = var.environment
   }
 }
 
@@ -20,4 +20,24 @@ module "report_lambda_function" {
   environment    = terraform_data.locals.output
   timeout        = "29"
   memory_size    = "3008"
+}
+
+resource "aws_cloudwatch_event_rule" "sequence_performance_report_schedule" {
+  name                = "sequence_performance_report_schedule_${var.environment}"
+  description         = "Scheduled Sequence Performance Report Lambda function"
+  schedule_expression = "cron(0 16 ? * 2 *)"
+}
+
+resource "aws_cloudwatch_event_target" "sequence_performance_report_lambda_target" {
+  rule      = aws_cloudwatch_event_rule.sequence_performance_report_schedule.name
+  target_id = "sequence_performance_report_lambda_target"
+  arn       = module.report_lambda_function.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_invoke_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = module.report_lambda_function.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.sequence_performance_report_schedule.arn
 }
