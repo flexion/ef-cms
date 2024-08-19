47982ae2cdb2e6909620c3ea9bc52d540841e0ea
John Cruz <jcruz@flexion.us>
2024-08-19 13:16:57 -0600

    10432: Update report string;

--- web-api/src/lambdas/SequencePerformanceReport/index.ts
+++ web-api/src/lambdas/SequencePerformanceReport/index.ts
@@ -1,16 +1,43 @@
 const AWS = require('aws-sdk');
 const axios = require('axios');
 const secretsManager = new AWS.SecretsManager({ region: 'us-east-1' });
-//web-api/src/lambdas
+
 exports.handler = async () =>
   await SequencePerformanceReport({
     httpClient: axios,
     secretsManagerClient: secretsManager,
   });
 
-async function getReportData(): Promise<string> {
-  return await new Promise(resolve => resolve('John is the BEST!!!!'));
+async function getReportData(): Promise<SequenceMetric[]> {
+  const TEMP_DATA: SequenceMetric[] = [];
+  for (let index = 0; index < 5; index++) {
+    TEMP_DATA.push({
+      duration: Math.floor(Math.random() * 100),
+      sequenceName: `sequenceName${index + 1}`,
+    });
+  }
+
+  TEMP_DATA.sort((a, b) => b.duration - a.duration);
+  return await new Promise(resolve => resolve(TEMP_DATA));
 }
+
+function formatReprtData(REPORT_DATA: SequenceMetric[]): string {
+  const REPORT_STRING: string = REPORT_DATA.reduce(
+    (acc, currentMetric, index) => {
+      const { duration, sequenceName } = currentMetric;
+      acc += `${index + 1}.\t${sequenceName} (${duration} seconds)\n`;
+      return acc;
+    },
+    '',
+  );
+
+  return REPORT_STRING;
+}
+
+type SequenceMetric = {
+  sequenceName: string;
+  duration: number;
+};
 
 type SequencePerformanceReportParams = {
   httpClient: { post: (url: string, body: any, options: any) => Promise<void> };
@@ -41,8 +68,28 @@ async function SequencePerformanceReport({
 
   if (!SLACK_WEBHOOK_URL) return;
 
-  const REPORT_DATA = await getReportData();
-  const BODY = { text: REPORT_DATA };
+  const REPORT_DATA: SequenceMetric[] = await getReportData();
+  const REPORT_STRING: string = formatReprtData(REPORT_DATA);
+
+  const BODY = {
+    blocks: [
+      {
+        text: {
+          text: `:bar_chart: *_Weekly Sequence Performance Report (${STAGE.toUpperCase()})_* :bar_chart:`,
+          type: 'mrkdwn',
+        },
+        type: 'section',
+      },
+      {
+        text: {
+          text: REPORT_STRING,
+          type: 'mrkdwn',
+        },
+        type: 'section',
+      },
+    ],
+  };
+
   const OPTIONS = {
     headers: {
       'Content-Type': 'application/json',
