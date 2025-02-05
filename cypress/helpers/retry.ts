2f27dc43615e826fc196c47378020c468ba478ba
Andy Kuny <akuny@flexion.us>
2025-02-05 14:14:38 -0500

    devex: rework retry helper for cypress

--- cypress/helpers/retry.ts
+++ cypress/helpers/retry.ts
@@ -1,12 +1,15 @@
-export function retry(cb: () => Cypress.Chainable<boolean>, maxAttempts = 5) {
+export function retry(
+  cb: () => Cypress.Chainable<boolean>,
+  maxAttempts = 5,
+): Cypress.Chainable {
   if (maxAttempts > 0) {
-    cb().then((isDone: boolean) => {
+    return cb().then((isDone: boolean) => {
       if (!isDone) {
         // eslint-disable-next-line cypress/no-unnecessary-waiting
         cy.wait(2000);
-        retry(cb, maxAttempts - 1);
+        return retry(cb, maxAttempts - 1);
       } else {
-        cy.log('retry condition passed, found expected elements');
+        return cy.log('retry condition passed, found expected elements');
       }
     });
   } else {
