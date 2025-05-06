1a9da94f72e50ea9786b8bbf9dc1ebfbde9eb40c
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-05-06 15:25:15 -0500

    10502: try simply try-catching db connection with backoff

--- web-api/src/getConnection.ts
+++ web-api/src/getConnection.ts
@@ -6,16 +6,45 @@ import { environment } from './environment';
 import fs from 'fs';
 
 let dbInstance: Promise<Kysely<Database>> | null = null;
+function delay(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export async function getConnection<T>({
   cb,
+  retries = 2,
+  backoffMs = 200,
 }: {
   cb: (r: Kysely<Database>) => T;
+  retries: number;
+  backoffMs: number;
 }): Promise<T> {
-  if (!dbInstance) {
-    dbInstance = establishConnection();
+  let attempt = 0;
+
+  while (true) {
+    try {
+      if (!dbInstance) {
+        dbInstance = establishConnection();
+      }
+      const awaitedInstance = await dbInstance;
+      return await cb(awaitedInstance);
+    } catch (err) {
+      if (isConnectionError(err) && attempt < retries) {
+        attempt++;
+        console.warn(
+          `pg connection error (attempt ${attempt} of ${retries}):`,
+          err,
+        );
+
+        dbInstance = establishConnection();
+
+        await delay(backoffMs * 2 ** (attempt - 1));
+        continue;
+      }
+
+      throw err;
+    }
   }
-  const awaitedInstance = await dbInstance;
-  return await cb(awaitedInstance);
 }
 
 async function establishConnection(): Promise<Kysely<Database>> {
@@ -70,6 +99,43 @@ function getPool(): PoolConfig {
     };
   }
   return pool;
+}
+
+// only retry on errors that will be fixed by reconnection
+// error codes not always available, therefore use combination of pg codes, node codes, and message string matching
+function isConnectionError(err: unknown): boolean {
+  if (!(err instanceof Error)) return false;
+
+  const pgCode = (err as any).code;
+  const nodeCode = (err as any).errno || (err as any).code;
+
+  const pgCodes = [
+    '28P01', // invalid_password (expired token)
+    '57P03', // cannot_connect_now
+    '53300', // too_many_connections (should never happen)
+    '08006', // connection_failure
+    '08003', // connection_does_not_exist
+    '08001', // sqlclient_unable_to_establish_connection
+  ];
+
+  const nodeCodes = [
+    'ECONNRESET',
+    'ECONNREFUSED',
+    'ETIMEDOUT',
+    'EPIPE',
+    'EHOSTUNREACH',
+    'ENOTFOUND',
+  ];
+
+  const errorMessage = err.message?.toLowerCase();
+
+  return (
+    (pgCode && pgCodes.includes(pgCode)) ||
+    (nodeCode && nodeCodes.includes(nodeCode)) ||
+    errorMessage.includes('password authentication failed') ||
+    errorMessage.includes('server closed the connection unexpectedly') ||
+    errorMessage.includes('terminating connection')
+  );
 }
 
 // async function isConnectionValid(db: Kysely<Database>): Promise<boolean> {
