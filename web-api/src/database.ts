185397cc5912163d9326c38602c4509091133d4c
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-05-15 16:01:12 -0500

    10568: staging with east routing

--- web-api/src/database.ts
+++ web-api/src/database.ts
@@ -4,11 +4,18 @@ import {
   Kysely,
   PostgresDialect,
 } from 'kysely';
-import { Database } from './database-types';
+import { Database, DatabaseSchema } from './database-schema';
 import { Pool } from 'pg';
 import { Signer } from '@aws-sdk/rds-signer';
 import { environment } from './environment';
 import fs from 'fs';
+import { openSearchGateway } from '@web-api/gateways/openSearch/openSearchGateway';
+import { formatNow } from '@shared/business/utilities/DateHandler';
+import { getLogger } from '@web-api/utilities/logger/getLogger';
+import {
+  OpenSearchSyncMessage,
+  OpenSearchSyncMessageType,
+} from '@web-api/lambdas/openSearch/openSearchSyncHandler';
 
 export const POOL = {
   ...environment.rds.pool,
@@ -19,15 +26,9 @@ export const POOL = {
     : undefined,
 };
 
-const dbInstances: Record<string, Kysely<Database> | null> = {
-  reader: null,
-  writer: null,
-};
+let dbInstance: Kysely<Database> | null = null;
 
-const tokens: Record<string, string | null> = {
-  'us-east-1': null,
-  'us-west-1': null,
-};
+let dbToken: string | null = null;
 
 export function connect(pool) {
   return new Kysely<Database>({
@@ -38,11 +39,11 @@ export function connect(pool) {
   });
 }
 
-async function generateRDSAuthToken({ host, region }) {
+async function generateRDSAuthToken() {
   const signer = new Signer({
-    hostname: host,
+    hostname: environment.rds.pool.host,
     port: 5432,
-    region,
+    region: 'us-east-1', // 10502 TODO: After west is deleted use environment.region
     username: environment.rds.pool.user,
   });
 
@@ -51,69 +52,57 @@ async function generateRDSAuthToken({ host, region }) {
   return token;
 }
 
-function clearToken(region: string) {
-  tokens[region] = null;
+function clearToken() {
+  dbToken = null;
 }
 
-async function getToken(region: string, host: string) {
+async function getToken() {
   if (environment.nodeEnv !== 'production') {
     return environment.rds.pool.password;
   }
-  const token = tokens[region];
+  const token = dbToken;
 
   if (!token) {
-    const freshToken = await generateRDSAuthToken({
-      host,
-      region,
-    });
-    tokens[region] = freshToken;
+    const freshToken = await generateRDSAuthToken();
+    dbToken = freshToken;
   }
 
-  return tokens[region];
+  return dbToken;
 }
 
 async function getConnection<T>({
   cb,
-  dbKey,
-  host,
-  region,
 }: {
-  dbKey: string;
   cb: (r: Kysely<Database>) => T;
-  region: string;
-  host: string;
 }): Promise<T> {
   try {
-    if (dbInstances[dbKey] && (await isConnectionValid(dbInstances[dbKey]))) {
+    if (dbInstance && (await isConnectionValid(dbInstance))) {
       // If valid, use the existing connection
-      return await cb(dbInstances[dbKey]);
+      return await cb(dbInstance);
     }
 
-    const token = await getToken(region, host);
+    const token = await getToken();
 
     if (!token) {
       throw new Error('token does not exist');
     }
 
-    dbInstances[dbKey] = connect({
+    dbInstance = connect({
       ...POOL,
-      host,
       password: token,
     });
 
-    return await cb(dbInstances[dbKey]!);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return await cb(dbInstance);
   } catch (err) {
-    clearToken(region);
-    const token = await getToken(region, host);
+    clearToken();
+    const token = await getToken();
 
-    dbInstances[dbKey] = connect({
+    dbInstance = connect({
       ...POOL,
-      host,
       password: token,
     });
 
-    return await cb(dbInstances[dbKey]!);
+    return await cb(dbInstance);
   }
 }
 
@@ -131,20 +120,49 @@ async function isConnectionValid(db: Kysely<Database>): Promise<boolean> {
 export function getDbReader<T>(cb: (r: Kysely<Database>) => T): Promise<T> {
   return getConnection({
     cb,
-    dbKey: 'reader',
-    host:
-      environment.region === 'us-west-1'
-        ? environment.rds.readHost
-        : environment.rds.pool.host,
-    region: environment.region,
   });
 }
 
-export function getDbWriter<T>(cb: (r: Kysely<Database>) => T): Promise<T> {
+function executeWriter<T>(cb: (r: Kysely<Database>) => T): Promise<T> {
   return getConnection({
     cb,
-    dbKey: 'writer',
-    host: environment.rds.pool.host,
-    region: 'us-east-1',
   });
+}
+
+// Prefer pgInsertInto, pgUpdateTable, pgDeleteFrom, etc.
+export async function getDbWriter<T>({
+  cb,
+  table,
+}: {
+  cb: (db: Kysely<Database>) => Promise<T>;
+  table: keyof Database | null;
+}): Promise<T> {
+  if (!table || !DatabaseSchema[table].indexOpenSearchMessage) {
+    return await executeWriter(cb);
+  }
+
+  const rawResult: T = await executeWriter(cb);
+  let result: any = rawResult;
+  if (DatabaseSchema[table].transformOpenSearchMessage) {
+    result = DatabaseSchema[table].transformOpenSearchMessage(rawResult);
+  }
+
+  if (result) {
+    try {
+      const message: OpenSearchSyncMessage = {
+        timestamp: formatNow(),
+        payload: result,
+        type: table as OpenSearchSyncMessageType,
+      };
+
+      await openSearchGateway().queueSync({ message });
+    } catch (err) {
+      getLogger().error(
+        'Error queuing message for opensearch from postgres',
+        err,
+      );
+    }
+  }
+
+  return rawResult;
 }
