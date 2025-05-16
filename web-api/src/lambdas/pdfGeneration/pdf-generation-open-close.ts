7674b1fb26bda5c51f6c6e9b807b0575ce899c0c
TomElliottFlexion <66225176+TomElliottFlexion@users.noreply.github.com>
2025-05-16 09:50:09 -0500

    testing lambda

---
+++ web-api/src/lambdas/pdfGeneration/pdf-generation-open-close.ts
@@ -0,0 +1,34 @@
+import { logLambdaStats } from '@web-api/lambdas/pdfGeneration/lambdaStats';
+
+export const openAndCloseAlot = async () => {
+  process.env.DEBUG = 'puppeteer:*,chrome:*';
+  process.env.PUPPETEER_LOGLEVEL = 'debug';
+
+  logLambdaStats('About to get chromium browser');
+  try {
+    for (let index = 0; index < 30; index++) {
+      const browser = await getChromiumBrowserAWS();
+      logLambdaStats(`Created chromium browser ${index}`);
+      await Promise.race([browser.close(), browser.close(), browser.close()]);
+      logLambdaStats(`closed browser ${index}`);
+    }
+  } catch (err) {
+    logLambdaStats('launch error');
+    throw err;
+  }
+  logLambdaStats('exiting');
+};
+
+const getChromiumBrowserAWS = async () => {
+  // we need to import these as external dependencies to allow us to reuse the application
+  // context in lambdas that DO NOT have the layer.
+  const { default: chromium } = await import('@sparticuz/chromium');
+  const { default: puppeteerCore } = await import('puppeteer-core');
+
+  return await puppeteerCore.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless as 'shell' | boolean,
+  });
+};
