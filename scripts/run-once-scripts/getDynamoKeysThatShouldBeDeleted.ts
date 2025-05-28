35339dd0d077ac24f5ed43678d237a0badbc42e1
Christopher Bisom <cbisom@flexion.us>
2025-05-28 10:51:05 -0400

    WIP: save whole object to file, not just ids

--- scripts/run-once-scripts/getDynamoKeysThatShouldBeDeleted.ts
+++ scripts/run-once-scripts/getDynamoKeysThatShouldBeDeleted.ts
@@ -1,41 +1,5 @@
-import { getDbReader } from '@web-api/database';
-import { createReadStream, readFileSync, writeFileSync } from 'fs';
+import { createReadStream, writeFileSync } from 'fs';
 import readline from 'readline';
-
-function getCaseDeadlinesInDynamo() {
-  const ids: string[] = [];
-
-  const rl = readline.createInterface({
-    input: createReadStream('/Users/jimbo/Documents/allDynamoRecords.txt'),
-    crlfDelay: Infinity,
-  });
-
-  rl.on('line', line => {
-    const obj = JSON.parse(line);
-    if (obj.sk.startsWith('case-deadline')) {
-      ids.push(obj.caseDeadlineId);
-    }
-  });
-
-  rl.on('close', () => {
-    console.log('Done!');
-    writeFileSync('./caseDeadlineIds.json', JSON.stringify(ids));
-  });
-}
-
-async function getCaseDeadlineIdsNotInPostgres() {
-  const dynamoIds = JSON.parse(readFileSync('./caseDeadlineIds.json', 'utf-8'));
-  const postgresIds = (
-    await getDbReader(reader =>
-      reader
-        .selectFrom('dwCaseDeadline')
-        .select('caseDeadlineId')
-        .where('caseDeadlineId', 'not in', dynamoIds)
-        .execute(),
-    )
-  ).map(cd => cd.caseDeadlineId);
-  console.log(postgresIds);
-}
 
 const objectsThatShouldNotBeInDynamo = [
   {
@@ -81,7 +45,7 @@ for (const obj of objectsThatShouldNotBeInDynamo) {
 }
 
 const uniqueKeysMap: Map<string, number> = new Map();
-const ids: string[] = [];
+const thingsToDelete: any[] = [];
 
 const rl = readline.createInterface({
   input: createReadStream('/Users/jimbo/Documents/allDynamoRecords.txt'),
@@ -96,12 +60,12 @@ rl.on('line', line => {
   if (lookupTable[lookupKey]) {
     const currentCount = uniqueKeysMap.get(lookupKey) || 0;
     uniqueKeysMap.set(lookupKey, currentCount + 1);
-    ids.push(obj.pk + '_' + obj.sk);
+    thingsToDelete.push(obj);
   }
 });
 
 rl.on('close', () => {
   console.log('Done!');
   console.log(uniqueKeysMap);
-  writeFileSync('./idsOfThingsInDynamo.json', JSON.stringify(ids));
+  writeFileSync('./idsOfThingsInDynamo.json', JSON.stringify(thingsToDelete));
 });
